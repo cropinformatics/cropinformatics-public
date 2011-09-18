4d74182cb38cefae08be69045d274e7d8338d9ea
Guy Davenport <daveneti@gmail.com>
2011-09-18 12:56:33 +0200

    initial commit

---
+++ org.cropinformatics.sql.tests/src/org/cropinformatics/sql/tests/HibernateDAOTest.java
@@ -0,0 +1,395 @@
+package org.cropinformatics.sql.tests;
+
+import org.cropinformatics.common.tests.BaseTest;
+import org.cropinformatics.sql.HibernateDAO;
+import org.junit.Test;
+
+public abstract class HibernateDAOTest extends BaseTest
+{
+	@Test
+	public void testHibernateDAODatabaseProperties()
+	{
+
+	}
+
+	@Test
+	public void testHibernateDAODatabasePropertiesSQLPropertyUtilities()
+	{
+
+	}
+
+	@Test
+	public void testGetSqlUtitlites()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQueryString()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQueryStringStringArrayObjectArray()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQueryStringIntegerInteger()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQueryStringStringArrayObjectArrayIntegerInteger()
+	{
+
+	}
+
+	@Test
+	public void testExecuteSQLQueryString()
+	{
+
+	}
+
+	@Test
+	public void testExecuteSQLQueryStringStringArrayObjectArray()
+	{
+
+	}
+
+	@Test
+	public void testExecuteSQLQueryStringIntegerInteger()
+	{
+
+	}
+
+	@Test
+	public void testExecuteSQLQueryStringStringArrayObjectArrayIntegerInteger()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQL()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLObject()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLObjectArray()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLObjectBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLObjectArrayBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLIntegerInteger()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLObjectIntegerInteger()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLObjectArrayIntegerInteger()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLObjectIntegerIntegerBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteQuerySQLObjectArrayIntegerIntegerBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdateString()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdateStringStringArrayObjectArray()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdateStringSqlParameterArrayObjectArrayBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteSQLUpdateString()
+	{
+
+	}
+
+	@Test
+	public void testExecuteSQLUpdateStringStringArrayObjectArray()
+	{
+
+	}
+
+	@Test
+	public void testExecuteSQLUpdateStringSqlParameterArrayObjectArrayBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdateSQL()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdateSQLObjectBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdateSQLObjectArrayBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdatesSQLFile()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdatesSQLFileObjectBoolean()
+	{
+
+	}
+
+	@Test
+	public void testExecuteUpdatesSQLFileObjectArrayBoolean()
+	{
+
+	}
+
+	@Test
+	public void testGetQueryStringSQL()
+	{
+
+	}
+
+	@Test
+	public void testGetQueryStringSQLObject()
+	{
+
+	}
+
+	@Test
+	public void testGetQueryStringSQLObjectArray()
+	{
+
+	}
+
+	@Test
+	public void testBeginTransaction()
+	{
+
+	}
+
+	@Test
+	public void testCommitTransaction()
+	{
+
+	}
+
+	@Test
+	public void testRollbackTransaction()
+	{
+
+	}
+
+	@Test
+	public void testGetDatabaseProperties()
+	{
+
+	}
+
+	@Test
+	public void testPersist()
+	{
+
+	}
+
+	@Test
+	public void testUpdate()
+	{
+
+	}
+
+	@Test
+	public void testSaveOrUpdate()
+	{
+
+	}
+
+	@Test
+	public void testMerge()
+	{
+
+	}
+
+	@Test
+	public void testDelete()
+	{
+
+	}
+
+	@Test
+	public void testRead()
+	{
+
+	}
+
+	@Test
+	public void testReadWithLock()
+	{
+
+	}
+
+	@Test
+	public void testCreateCriteriaClass()
+	{
+
+	}
+
+	@Test
+	public void testCreateCriteriaDetachedCriteria()
+	{
+
+	}
+
+	@Test
+	public void testPersistQueryWithTransaction()
+	{
+
+	}
+
+	@Test
+	public void testUpdateWithTransaction()
+	{
+
+	}
+
+	@Test
+	public void testSaveOrUpdateWithTransaction()
+	{
+
+	}
+
+	@Test
+	public void testMergeWithTransaction()
+	{
+
+	}
+
+	@Test
+	public void testDeleteWithTransaction()
+	{
+
+	}
+
+	@Test
+	public void testCreateDatabaseProperties()
+	{
+
+	}
+
+	@Test
+	public void testClose()
+	{
+
+	}
+
+	@Test
+	public void testParseSQLFile()
+	{
+
+	}
+
+	@Test
+	public void testHasSQLGroup()
+	{
+
+	}
+
+	@Test
+	public void testGetSQLGroup()
+	{
+
+	}
+
+	@Test
+	public void testHasSQLQuery()
+	{
+
+	}
+
+	@Test
+	public void testGetSQLQuery()
+	{
+
+	}
+
+	@Test
+	public void testHasSQLFile()
+	{
+
+	}
+
+	@Test
+	public void testGetSQLFile()
+	{
+
+	}
+
+	@Test
+	public void testCreateSessionFactory()
+	{
+
+	}
+	
+	protected abstract HibernateDAO getDao();
+
+}
